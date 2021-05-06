db19436d4cc17ea4c4dcc6f2a8a4114442b7ba25
Sergey Toy <toydestroyer@users.noreply.github.com>
2021-05-07 00:01:25 +0800

    guard clause in searcher

--- telegram_webhooks/searcher.rb
+++ telegram_webhooks/searcher.rb
@@ -6,13 +6,10 @@ class Searcher
       # Get exact match
       exact_match = DataSource::CoinGecko.available_assets.select { |item| item['symbol'].downcase == query.downcase || item['name'].downcase == query.downcase }.first(10)
 
-      partial_match = []
+      return exact_match if exact_match.size == 10
 
-      # There can be a situation when more than 10 coins with the same symbol exist
-      if exact_match.size < 10
-        exact_ids = exact_match.map { |item| item['id'] }
-        partial_match = DataSource::CoinGecko.available_assets.select { |item| !exact_ids.include?(item['id']) && (item['symbol'].downcase.start_with?(query.downcase) || item['name'].downcase.start_with?(query.downcase)) }.first(10 - exact_match.size)
-      end
+      exact_ids = exact_match.map { |item| item['id'] }
+      partial_match = DataSource::CoinGecko.available_assets.select { |item| !exact_ids.include?(item['id']) && (item['symbol'].downcase.start_with?(query.downcase) || item['name'].downcase.start_with?(query.downcase)) }.first(10 - exact_match.size)
 
       exact_match + partial_match
     end

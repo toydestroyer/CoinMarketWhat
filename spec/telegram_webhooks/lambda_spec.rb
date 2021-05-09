3d5292cd3818f1f419ca5e3544c064e7f7b154a9
Sergey Toy <toydestroyer@users.noreply.github.com>
2021-05-09 23:59:30 +0800

    add generic webhook specs

--- spec/telegram_webhooks/lambda_spec.rb
+++ spec/telegram_webhooks/lambda_spec.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
 RSpec.describe Lambda do
-  describe 'Webhooks' do
-    let(:event) do
-      {
-        'body' => '{}'
-      }
-    end
+  describe '#webhook' do
+    subject(:result) { described_class.webhook(event: event, context: nil) }
 
-    it 'works' do
-      expect { described_class.webhook(event: event, context: {}) }.not_to raise_error
+    let(:event) { { 'body' => body.to_json } }
+
+    context 'with any event' do
+      let(:body) { { random: 'param' } }
+
+      before do
+        allow(RequestLogger).to receive(:enqueue)
+      end
+
+      it 'not raise an error' do
+        expect { result }.not_to raise_error
+      end
+
+      it 'returns 200 ok' do
+        expect(result).to include(statusCode: 200, body: 'ok')
+      end
+
+      it 'sends log to queue' do
+        result
+
+        expect(RequestLogger).to have_received(:enqueue).with({ 'random' => 'param' })
+      end
     end
   end
 end

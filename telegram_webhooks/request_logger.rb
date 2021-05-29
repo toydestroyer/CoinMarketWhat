96ce42d8d63da88e815f4805bde0dc04a7692547
Sergey Toy <toydestroyer@users.noreply.github.com>
2021-05-29 10:29:31 +0800

    start cleaning up

--- telegram_webhooks/request_logger.rb
+++ telegram_webhooks/request_logger.rb
@@ -12,31 +12,8 @@ class RequestLogger
 
   def self.save(logs)
     logs.each do |log|
-      body = JSON.parse(log['body'])
-      update_id = body.delete('update_id')
-
-      raise 'missing update_id' unless update_id
-      raise 'only 1 key can be processed' unless body.keys.size == 1
-
-      event_type = body.keys.first
-      raise "unsupported event_type: #{event_type}" unless allowed_events.include?(event_type)
-
-      time = Time.at(0, log['attributes']['SentTimestamp'].to_i, :millisecond)
-
-      Lambda.s3.put_object(
-        body: body[event_type].to_json,
-        bucket: ENV['LOGS_BUCKET'],
-        key: "#{event_type}/#{time_prefix(time)}/#{update_id}.json"
-      )
+      event_log = EventLog.new(log)
+      event_log.save
     end
-  end
-
-  def self.time_prefix(time)
-    time.strftime('%Y/%m/%d/%H')
-  end
-
-  # There are much more possible events, but I want to be in control of which one to store
-  def self.allowed_events
-    %w[callback_query chosen_inline_result inline_query message]
   end
 end
--- telegram_webhooks/searcher.rb
+++ telegram_webhooks/searcher.rb
@@ -5,15 +5,31 @@ class Searcher
     def call(query:)
       return top if query.empty?
 
-      # Get exact match
-      exact_match = DataSource::CoinGecko.available_assets.select { |_k, v| v['symbol'].casecmp(query).zero? || v['name'].casecmp(query).zero? }.first(10).to_h
+      exact_match = get_exact_match(query: query)
 
       return exact_match if exact_match.size == 10
 
-      exact_ids = exact_match.map { |k, _v| k }
-      partial_match = DataSource::CoinGecko.available_assets.select { |k, v| !exact_ids.include?(k) && (v['symbol'].start_with?(query.upcase) || v['name'].downcase.start_with?(query.downcase)) }.first(10 - exact_match.size).to_h
+      partial_match = get_partial_match(query: query, skip: exact_match.keys)
 
       exact_match.merge(partial_match)
+    end
+
+    private
+
+    def get_exact_match(query:)
+      result = DataSource::CoinGecko.available_assets.select do |_k, v|
+        v['symbol'].casecmp(query).zero? || v['name'].casecmp(query).zero?
+      end
+
+      result.first(10).to_h
+    end
+
+    def get_partial_match(query:, skip:)
+      result = DataSource::CoinGecko.available_assets.select do |k, v|
+        !skip.include?(k) && (v['symbol'].start_with?(query.upcase) || v['name'].downcase.start_with?(query.downcase))
+      end
+
+      result.first(10 - skip.size).to_h
     end
 
     def top

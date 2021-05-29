0a37b676cc798ac7545221a8af96a7a3918e2850
Sergey Toy <toydestroyer@users.noreply.github.com>
2021-05-29 12:18:19 +0800

    cleanup handlers

--- telegram_webhooks/handler/base.rb
+++ telegram_webhooks/handler/base.rb
@@ -23,27 +23,7 @@ module Handler
       selected_ids = selected.keys
       prices = DataSource::CoinGecko.prices(ids: selected_ids, quote: 'USD')
 
-      result = prices.map do |symbol|
-        price = Money.from_amount(symbol['current_price'], 'USD').format
-
-        title = "#{symbol['name']} (#{symbol['symbol'].upcase})"
-
-        initial_state = decompose_callback_data("#{symbol['id']}:coingecko[0]:USD[0]")
-
-        {
-          type: :article,
-          id: SecureRandom.hex,
-          title: title,
-          description: "#{price} @ CoinGecko",
-          thumb_url: symbol['image'],
-          thumb_width: 250,
-          thumb_height: 250,
-          input_message_content: {
-            message_text: "#{title} — #{price}"
-          },
-          reply_markup: build_reply_markup(initial_state)
-        }
-      end
+      result = prices.map { |symbol| render_inline_query_item(symbol) }
 
       result.to_json
     end
@@ -71,41 +51,86 @@ module Handler
 
     private
 
+    def render_inline_query_item(symbol)
+      price = Money.from_amount(symbol['current_price'], 'USD').format
+      title = "#{symbol['name']} (#{symbol['symbol'].upcase})"
+      initial_state = decompose_callback_data("#{symbol['id']}:coingecko[0]:USD[0]")
+
+      {
+        type: :article,
+        id: SecureRandom.hex,
+        title: title,
+        description: "#{price} @ CoinGecko",
+        thumb_url: symbol['image'],
+        thumb_width: 250,
+        thumb_height: 250,
+        input_message_content: {
+          message_text: "#{title} — #{price}"
+        },
+        reply_markup: build_reply_markup(initial_state)
+      }
+    end
+
     def build_pairs_row(state)
       data_source = Lambda.data_sources_map[state[:source]]
       avaliable_pairs = data_source.pairs(id: state[:base])
 
-      pagination = false
+      paginated, avaliable_pairs = paginate(list: avaliable_pairs, offset: state[:quote_offset])
 
-      # Due to display concerns, I want to limit the number of buttons in the row to 4
-      # If available pairs more than 4 then only 3 will be displayed along with the pagination arrow
-      if avaliable_pairs.size > 4
-        avaliable_pairs = avaliable_pairs[state[:quote_offset], 3]
-        pagination = true
-      end
+      pairs = avaliable_pairs.map { |item| build_pair_button(item: item, state: state) }
 
-      pairs = avaliable_pairs.map do |item|
-        { text: item == state[:quote] ? "• #{item} •" : item, callback_data: "#{state[:base]}:#{state[:source]}[#{state[:source_offset]}]:#{item}[#{state[:quote_offset]}]" }
-      end
-
-      pairs << { text: '→', callback_data: "#{state[:base]}:#{state[:source]}[#{state[:source_offset]}]:#{state[:quote]}[#{state[:quote_offset] + 1}]" } if pagination
+      pairs << pagination_button(state: state, row: 'quote') if paginated
 
       pairs
     end
 
+    def build_pair_button(item:, state:)
+      {
+        text: item == state[:quote] ? "• #{item} •" : item,
+        callback_data: "#{state[:base]}:#{state[:source]}[#{state[:source_offset]}]:#{item}[#{state[:quote_offset]}]"
+      }
+    end
+
     def build_data_sources_row(state)
       DataSource::CoinGecko.available_assets[state[:base]]['tickers'].keys.map do |item|
-        data_source = Lambda.data_sources_map[item]
-
-        if item == state[:source] # selected
-          text = "• #{data_source.display_name} •"
-          quote = "#{state[:quote]}[#{state[:quote_offset]}]"
-        else
-          text = data_source.display_name
-          quote = "#{data_source.pairs(id: state[:base])[0]}[0]"
-        end
-        { text: text, callback_data: "#{state[:base]}:#{item}[#{state[:source_offset]}]:#{quote}" }
+        build_data_source_button(item: item, state: state)
       end
+    end
+
+    def build_data_source_button(item:, state:)
+      data_source = Lambda.data_sources_map[item]
+
+      if item == state[:source] # selected
+        text = "• #{data_source.display_name} •"
+        quote = "#{state[:quote]}[#{state[:quote_offset]}]"
+      else
+        text = data_source.display_name
+        quote = "#{data_source.pairs(id: state[:base])[0]}[0]"
+      end
+
+      { text: text, callback_data: "#{state[:base]}:#{item}[#{state[:source_offset]}]:#{quote}" }
+    end
+
+    def paginate(list:, offset:, limit: 4)
+      # Due to display concerns, I want to limit the number of buttons in the row to 4
+      # If available pairs more than 4 then only 3 will be displayed along with the pagination arrow
+      if list.size > limit
+        list = list[offset, limit - 1]
+        return true, list
+      end
+
+      [false, list]
+    end
+
+    def pagination_button(state:, row:)
+      state["#{row}_offset".to_sym] += 1
+      source = "#{state[:source]}[#{state[:source_offset]}]"
+      quote = "#{state[:quote]}[#{state[:quote_offset]}]"
+
+      {
+        text: '→',
+        callback_data: "#{state[:base]}:#{source}:#{quote}"
+      }
     end
   end
 end
--- telegram_webhooks/handler/callback_query.rb
+++ telegram_webhooks/handler/callback_query.rb
@@ -7,19 +7,28 @@ module Handler
       data_source = Lambda.data_sources_map[current_state[:source]]
 
       symbol = data_source.prices(ids: [current_state[:base]], quote: current_state[:quote])[0]
-      price = begin
-        Money.from_amount(symbol['current_price'], current_state[:quote]).format
-      rescue Money::Currency::UnknownCurrency => _e
-        "#{symbol['current_price']} #{current_state[:quote]}"
-      end
+      price = render_price(amount: symbol['current_price'], quote: current_state[:quote])
 
+      edit_message(symbol: symbol, price: price, state: current_state)
+    end
+
+    def render_price(amount:, quote:)
+      Money.from_amount(amount, quote).format
+    rescue Money::Currency::UnknownCurrency => _e
+      "#{amount} #{quote}"
+    end
+
+    def edit_message(symbol:, price:, state:)
       title = "#{symbol['name']} (#{symbol['symbol'].upcase})"
 
-      RestClient.get("https://api.telegram.org/bot#{token}/editMessageText", params: {
-                       text: "#{title} — #{price}",
-                       inline_message_id: query['inline_message_id'],
-                       reply_markup: build_reply_markup(current_state).to_json
-                     })
+      RestClient.get(
+        "https://api.telegram.org/bot#{token}/editMessageText",
+        params: {
+          text: "#{title} — #{price}",
+          inline_message_id: query['inline_message_id'],
+          reply_markup: build_reply_markup(state).to_json
+        }
+      )
     end
   end
 end

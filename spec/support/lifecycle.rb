07fccf1effb31aa5cda67406af4cc7f3b34d47ec
Sergey Toy <toydestroyer@users.noreply.github.com>
2021-06-09 20:16:29 +0800

    add specs for searcher

--- spec/support/lifecycle.rb
+++ spec/support/lifecycle.rb
@@ -4,27 +4,38 @@ RSpec.configure do |config|
   config.before(:suite) do
     Lambda.sqs.create_queue(queue_name: 'CoinMarketWhatLogsQueue')
     Lambda.s3.create_bucket(bucket: ENV['LOGS_BUCKET'])
+    Lambda.s3.create_bucket(bucket: ENV['CACHE_BUCKET'])
+    Lambda.s3.put_object(key: 'coingecko.json', bucket: ENV['CACHE_BUCKET'], body: file_fixture('coingecko.json'))
   end
 
   config.after(:suite) do
     Lambda.sqs.delete_queue(queue_url: ENV['LOGS_QUEUE'])
     Lambda.s3.delete_bucket(bucket: ENV['LOGS_BUCKET'])
+
+    empty_bucket(ENV['CACHE_BUCKET'])
+    Lambda.s3.delete_bucket(bucket: ENV['CACHE_BUCKET'])
   end
 
   config.after do
+    empty_bucket(ENV['LOGS_BUCKET'])
+
     # Remove all objects across all buckets after each example
-    Lambda.s3.list_buckets.buckets.each do |bucket|
-      Lambda.s3.list_objects(bucket: bucket.name).contents.each do |file|
-        Lambda.s3.delete_object(
-          bucket: bucket.name,
-          key: file.key
-        )
-      end
-    end
+    # Lambda.s3.list_buckets.buckets.each do |bucket|
+    #   empty_bucket(bucket.name)
+    # end
 
     # Remove all messages across all queues after each example
     # Lambda.sqs.list_queues.queue_urls.each do |queue_url|
     #   Lambda.sqs.purge_queue(queue_url: queue_url)
     # end
+  end
+
+  def empty_bucket(name)
+    Lambda.s3.list_objects(bucket: name).contents.each do |file|
+      Lambda.s3.delete_object(
+        bucket: name,
+        key: file.key
+      )
+    end
   end
 end
--- spec/telegram_webhooks/searcher_spec.rb
+++ spec/telegram_webhooks/searcher_spec.rb
@@ -1,5 +1,53 @@
 # frozen_string_literal: true
 
 RSpec.describe Searcher do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:op_result) { described_class.call(query: query) }
+
+  context 'with empty query' do
+    let(:query) { '' }
+
+    it 'returns top results' do
+      expect(op_result.keys).to contain_exactly('bitcoin', 'ethereum', 'bitcoin-cash')
+    end
+  end
+
+  context 'with exact match by symbol' do
+    let(:query) { 'BTC' }
+
+    it 'returns proper results' do
+      expect(op_result.keys).to contain_exactly('bitcoin')
+    end
+  end
+
+  context 'with partial match by symbol' do
+    let(:query) { 'B' }
+
+    it 'returns proper results' do
+      expect(op_result.keys).to contain_exactly('bitcoin', 'bitcoin-cash')
+    end
+  end
+
+  context 'with exact match by name' do
+    let(:query) { 'ethereum' }
+
+    it 'returns proper results' do
+      expect(op_result.keys).to contain_exactly('ethereum')
+    end
+  end
+
+  context 'with partial match by name' do
+    let(:query) { 'ether' }
+
+    it 'returns proper results' do
+      expect(op_result.keys).to contain_exactly('ethereum')
+    end
+  end
+
+  context 'with mixed match by name' do
+    let(:query) { 'bitcoin' }
+
+    it 'returns proper results' do
+      expect(op_result.keys).to contain_exactly('bitcoin', 'bitcoin-cash')
+    end
+  end
 end

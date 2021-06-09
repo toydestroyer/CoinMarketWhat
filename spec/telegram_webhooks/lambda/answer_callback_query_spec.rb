cb87023199321d8e201b9061189bbdd2826e5623
Sergey Toy <toydestroyer@users.noreply.github.com>
2021-06-09 23:17:30 +0800

    add answer callback query function spec

---
+++ spec/telegram_webhooks/lambda/answer_callback_query_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe Lambda::AnswerCallbackQuery, :with_lambda do
+  let(:event) { { 'Records' => [record] } }
+  let(:callback_query) { json_fixture('telegram/callback_query.json') }
+  let(:record) { { 'body' => body } }
+  let(:body) { callback_query['callback_query'].to_json }
+  let(:stub_url) { "https://api.telegram.org/bot#{ENV['TELEGRAM_BOT_API_TOKEN']}/answerCallbackQuery" }
+
+  before do
+    success = file_fixture('telegram/successful_response.json')
+
+    stub_request(:post, stub_url).to_return(status: 200, body: success)
+  end
+
+  it 'works' do
+    result
+
+    expect(a_request(:post, stub_url)).to have_been_made.once
+  end
+end
